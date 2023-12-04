32e65e45148f9b062b00e4362e4d0c6104afa5b4
AnnaLevitski <99040149+AnnaLevitski@users.noreply.github.com>
2023-12-04 16:21:50 +0200

    added xpath to elemaent

--- src/test/java/HW.java
+++ src/test/java/HW.java
@@ -18,21 +18,38 @@ public class HW {
     @Test
     public void test(){
         WebElement elHomeNav = wd.findElement(By.cssSelector("[href='/home']"));
+        WebElement elHomeNav2 = wd.findElement(By.xpath("//div[@class='navbar-component_nav__1X_4m']/a[1]"));
+
         WebElement elAboutNav = wd.findElement(By.cssSelector("[href='/about']"));
+        WebElement elAboutNav2 = wd.findElement(By.xpath("//div[@class='navbar-component_nav__1X_4m']/a[2]"));
+
+
         WebElement elLogInNav = wd.findElement(By.cssSelector("[href='/login']"));
+        WebElement elLogInNav2 = wd.findElement(By.xpath("//div[@class='navbar-component_nav__1X_4m']/a[3]"));
+
 
         WebElement elEmailInput = wd.findElement(By.cssSelector("[placeholder='Email']"));
-        WebElement elPasswordInput = wd.findElement(By.cssSelector("[placeholder='Password']"));
+        WebElement elEmailInput2 = wd.findElement(By.xpath("//input[@placeholder='Email']"));
+
+
+
+        WebElement elPasswordInput = wd.findElement(By.xpath("//input[@placeholder='Password']"));
+
+
+
+        WebElement elRegistrationButton= wd.findElement(By.cssSelector("button[name='registration']"));
+        WebElement elRegistrationButton2= wd.findElement(By.xpath("//button[@name='registration']"));
+
 
         WebElement elLoginButton= wd.findElement(By.cssSelector("button[name='login']"));
-        WebElement elRegistrationButton= wd.findElement(By.cssSelector("button[name='registration']"));
+        WebElement elLoginButton2= wd.findElement(By.xpath("//button[@name='login']"));
 
 
     }
 
     @AfterClass
     public void close(){
-        wd.quit();
+       wd.quit();
     }
 
 
